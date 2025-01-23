5cb170d4cabafb849324b5f56f55c996ca40f5e1
Nourhan Essam <nourhan.essam.makhlouf@gmail.com>
2025-01-23 20:55:31 +0200

    initiate onion arch for backend

---
+++ Customer Support System Backend/CustomerSupport.Application/Services/Implementations/TicketService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerSupport.Application.Services.Implementations
+{
+    public class TicketService
+    {
+    }
+}
