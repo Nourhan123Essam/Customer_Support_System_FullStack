5cbf8c7b55b036861bda4d0d0faf08398ba5ed23
Nourhan Essam <nourhan.essam.makhlouf@gmail.com>
2025-03-14 11:53:15 +0200

    save the solution

--- Customer Support System Backend/CustomerSupport.Presentation/obj/Debug/net8.0/CustomerSupport.Presentation.AssemblyInfo.cs
+++ Customer Support System Backend/CustomerSupport.Presentation/obj/Debug/net8.0/CustomerSupport.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomerSupport.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be5218e6b906d2bb9090057774794527c378f85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5070ac7db75291f3d7041cffd3aeca8a02c91fd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomerSupport.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomerSupport.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
